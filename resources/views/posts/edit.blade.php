ab79369b3e413bbd04823884e004b042bf8ebef5
Tempuradonn <c1049703@planet.kanazawa-it.ac.jp>
2022-12-02 02:56:36 +0000

    practice8-5

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -8,22 +8,23 @@
     </head>
     <body class = "antialiased">
         <h1>Blog Name</h1>
-        <form action="/posts" method="POST">
+        <form action="/posts/{{ $post->id}}"method="POST">
             @csrf
+            @method("PUT")
             <div class = "title">
                 <h2>Title</h2>
-                <input type="text" name=post[title] placeholder="タイトル" value={{old('post.title')}}>
+                <input type="text" name=post[title] placeholder="タイトル" value={{( $post->title )}}>
                 <p class='title_error' style="color:blue">{{ $errors->first('post.title') }}</p>
             </div>
             <div class="body">
                 <h2>Body</h2>
-                <textarea name="post[body]" placeholder="入力してください。">{{old('post.body')}}</textarea>
-                <p class='body_error' style="color:blue">{{ $errors->first('post.body') }}</p>
+                <textarea name="post[body]" placeholder="入力してください。">{{( $post->body )}}</textarea>
+                <p class='body_error' style="color:blue">{{ $errors->first('post.body')}}</p>
             </div>
-            <input type="submit" value="store">
+            <input type="submit" value="update">
         </form>
             <div class='footer'>
-                <a href ="/">back</a>
+                <a href ="/posts/{{$post->id}}">戻る</a>
             </div>
         </body>
 </html>
